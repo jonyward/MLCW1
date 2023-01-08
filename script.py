51c5d53dab19215c1df159109b4f9dfed43f3250
jonyward <jonyward@gmail.com>
2023-01-08 22:52:24 +0000

    fixed imports

--- script.py
+++ script.py
@@ -3,8 +3,6 @@ import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split 
 from sklearn import metrics
-
-from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, precision_score
 from sklearn.preprocessing import MinMaxScaler, StandardScaler
 from sklearn.svm import SVC
